e483ec992b009d77b3ac29249e3cdf8c1e192448
pirosiki197 <lingmukangtai825@gmail.com>
2025-01-11 23:30:34 +0900

    printf

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,3 +1,56 @@
+fn printf(comptime fmt: []const u8, args: anytype) void {
+    const args_type = @typeInfo(@TypeOf(args));
+    if (args_type != .Struct) {
+        @compileError("print() requires a struct argument");
+    }
+    const fields_info = args_type.Struct.fields;
+
+    comptime var idx: usize = 0;
+    comptime var field_idx: usize = 0;
+    inline while (idx < fmt.len) : (idx += 1) {
+        if (fmt[idx] != '%') {
+            put_char(fmt[idx]);
+            continue;
+        }
+
+        idx += 1;
+        switch (fmt[idx]) {
+            '%' => put_char('%'),
+            's' => {
+                const field = @field(args, fields_info[field_idx].name);
+                for (field) |c| {
+                    put_char(c);
+                }
+                field_idx += 1;
+            },
+            'd' => {
+                var value: u32 = blk: {
+                    const field = @field(args, fields_info[field_idx].name);
+                    const _value: i32 = @intCast(field);
+                    if (_value < 0) {
+                        put_char('-');
+                        break :blk @intCast(-_value);
+                    }
+                    break :blk @intCast(_value);
+                };
+
+                var divisor: u32 = 1;
+                while (value / divisor >= 10) {
+                    divisor *= 10;
+                }
+
+                while (divisor > 0) {
+                    put_char('0' + @as(u8, @intCast(value / divisor)));
+                    value %= divisor;
+                    divisor /= 10;
+                }
+                field_idx += 1;
+            },
+            else => {},
+        }
+    }
+}
+
 const SbiRet = struct {
     err: usize,
     value: usize,
@@ -40,10 +93,9 @@ fn put_char(c: u8) void {
 }
 
 pub export fn kernel_main() void {
-    const msg = "\n\nHello World!\n";
-    for (msg) |c| {
-        put_char(c);
-    }
+    printf("Hello, world!\n", .{});
+    printf("Hello, %s!\n", .{"world"});
+    printf("1 + 2 = %d\n", .{3});
 
     while (true) {
         asm volatile ("wfi");
