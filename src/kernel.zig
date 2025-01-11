66abf73fc9daeeaaa50991f8b5246c996ee1617e
pirosiki197 <lingmukangtai825@gmail.com>
2025-01-11 23:46:12 +0900

    ファイル分割

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,98 +1,6 @@
-fn printf(comptime fmt: []const u8, args: anytype) void {
-    const args_type = @typeInfo(@TypeOf(args));
-    if (args_type != .Struct) {
-        @compileError("print() requires a struct argument");
-    }
-    const fields_info = args_type.Struct.fields;
-
-    comptime var idx: usize = 0;
-    comptime var field_idx: usize = 0;
-    inline while (idx < fmt.len) : (idx += 1) {
-        if (fmt[idx] != '%') {
-            put_char(fmt[idx]);
-            continue;
-        }
-
-        idx += 1;
-        switch (fmt[idx]) {
-            '%' => put_char('%'),
-            's' => {
-                const field = @field(args, fields_info[field_idx].name);
-                for (field) |c| {
-                    put_char(c);
-                }
-                field_idx += 1;
-            },
-            'd' => {
-                var value: u32 = blk: {
-                    const field = @field(args, fields_info[field_idx].name);
-                    const _value: i32 = @intCast(field);
-                    if (_value < 0) {
-                        put_char('-');
-                        break :blk @intCast(-_value);
-                    }
-                    break :blk @intCast(_value);
-                };
-
-                var divisor: u32 = 1;
-                while (value / divisor >= 10) {
-                    divisor *= 10;
-                }
-
-                while (divisor > 0) {
-                    put_char('0' + @as(u8, @intCast(value / divisor)));
-                    value %= divisor;
-                    divisor /= 10;
-                }
-                field_idx += 1;
-            },
-            'x' => {
-                const value: usize = @intCast(@field(args, fields_info[field_idx].name));
-                comptime var i: isize = 7;
-                inline while (i >= 0) : (i -= 1) {
-                    const nibble = (value >> (i * 4)) & 0xf;
-                    if (nibble < 10) {
-                        put_char('0' + @as(u8, nibble));
-                    } else {
-                        put_char('a' + @as(u8, nibble - 10));
-                    }
-                }
-            },
-            else => {},
-        }
-    }
-}
-
-fn print(s: []const u8) void {
-    for (s) |c| {
-        put_char(c);
-    }
-}
-
-const SbiRet = struct {
-    err: usize,
-    value: usize,
-};
-
-fn sbi_call(arg0: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize, arg5: usize, fid: usize, eid: usize) SbiRet {
-    var err: usize = 0;
-    var value: usize = 0;
-    _ = asm volatile (
-        \\ ecall
-        : [err] "={a0}" (err),
-          [value] "={a1}" (value),
-        : [arg0] "{a0}" (arg0),
-          [arg1] "{a1}" (arg1),
-          [arg2] "{a2}" (arg2),
-          [arg3] "{a3}" (arg3),
-          [arg4] "{a4}" (arg4),
-          [arg5] "{a5}" (arg5),
-          [fid] "{a6}" (fid),
-          [eid] "{a7}" (eid),
-        : "memory"
-    );
-    return SbiRet{ .err = err, .value = value };
-}
+const common = @import("common.zig");
+const print = common.print;
+const printf = common.printf;
 
 extern const __stack_top: [*]u8;
 extern const __bss: [*]u8;
@@ -104,10 +12,6 @@ fn memset(buf: [*]u8, c: u8, n: usize) [*]u8 {
         p[i] = c;
     }
     return buf;
-}
-
-fn put_char(c: u8) void {
-    _ = sbi_call(c, 0, 0, 0, 0, 0, 0, 1);
 }
 
 pub export fn kernel_main() void {
