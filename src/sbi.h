4a428efe96bbcd8de8c63b567795be37f79dc239
pirosiki197 <lingmukangtai825@gmail.com>
2025-02-16 13:25:38 +0900

    remove c files

--- src/sbi.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct sbiret {
-    long err;
-    long value;
-};
-
-struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
-                       long arg5, long fid, long eid);
